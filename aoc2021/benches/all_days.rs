05526f3a9266cfc81eeae14acdd7f62457f2de2f
Hendrik Schawe <surt91@gmail.com>
2021-12-06 17:45:52 +0100

    benchmark all solutions

---
+++ aoc2021/benches/all_days.rs
@@ -0,0 +1,24 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+#[macro_export]
+macro_rules! day_benches {
+    ($($name:ident,)*) => {
+        $(
+            fn $name(c: &mut Criterion) {
+                c.bench_function(stringify!($name), |b| b.iter(aoc2021::$name::run));
+            }
+        )*
+        criterion_group!(benches, $($name,)*);
+    }
+}
+
+day_benches!{
+    day01,
+    day02,
+    day03,
+    day04,
+    day05,
+    day06,
+}
+
+criterion_main!(benches);

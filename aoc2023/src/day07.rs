1d851873ed89e733dd7f9b83c4c107e644a1ad88
Hendrik Schawe <hendrik.schawe@codecentric.de>
2023-12-08 14:37:15 +0100

    day 7 b

--- aoc2023/src/day07.rs
+++ aoc2023/src/day07.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::str::FromStr;
 
 use aoc2021::data_str;
@@ -33,7 +34,60 @@ impl FromStr for Card {
     }
 }
 
-#[derive(Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, Clone, Hash, PartialEq, PartialOrd, Eq, Ord)]
+enum CardJ {
+    J,
+    Number(u8),
+    T,
+    Q,
+    K,
+    A,
+}
+
+impl FromStr for CardJ {
+    type Err = AdventError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" => Ok(CardJ::A),
+            "K" => Ok(CardJ::K),
+            "Q" => Ok(CardJ::Q),
+            "J" => Ok(CardJ::J),
+            "T" => Ok(CardJ::T),
+            "9" | "8" | "7" | "6" | "5" | "4" | "3" | "2" | "1" => Ok(CardJ::Number(s.parse().unwrap())),
+            _ => Err(AdventError::UnexpectedElement {
+                found: s.to_string(),
+                expected: &["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+            })
+        }
+    }
+}
+
+fn recognize_hand(cards: Vec<Card>) -> Hand {
+    let card_counter: Counter<Card> = cards.iter().cloned().collect();
+    match card_counter.len() {
+        1 => Hand::FiveOfAKind(cards),
+        2 => {
+            if card_counter.values().max().unwrap() == &4 {
+                Hand::FourOfAKind(cards)
+            } else {
+                Hand::FullHouse(cards)
+            }
+        },
+        3 => {
+            if card_counter.values().max().unwrap() == &3 {
+                Hand::ThreeOfAKind(cards)
+            } else {
+                Hand::TwoPair(cards)
+            }
+        },
+        4 => Hand::OnePair(cards),
+        5 => Hand::HighCard(cards),
+        _ => unreachable!()
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, PartialOrd, Eq, Ord)]
 enum Hand {
     HighCard(Vec<Card>),
     OnePair(Vec<Card>),
@@ -49,38 +103,84 @@ impl FromStr for Hand {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let cards: Vec<Card> = s.chars().map(|c| c.to_string().parse()).collect::<Result<_, AdventError>>()?;
-        let card_counter: Counter<Card> = cards.iter().cloned().collect();
-        match card_counter.len() {
-            1 => Ok(Hand::FiveOfAKind(cards)),
-            2 => {
-                if card_counter.values().max().unwrap() == &4 {
-                    Ok(Hand::FourOfAKind(cards))
-                } else {
-                    Ok(Hand::FullHouse(cards))
-                }
-            },
-            3 => {
-                if card_counter.values().max().unwrap() == &3 {
-                    Ok(Hand::ThreeOfAKind(cards))
-                } else {
-                    Ok(Hand::TwoPair(cards))
-                }
-            },
-            4 => Ok(Hand::OnePair(cards)),
-            5 => Ok(Hand::HighCard(cards)),
-            _ => unreachable!()
-        }
+        Ok(recognize_hand(cards))
     }
 }
 
-pub fn run() -> (usize, u64) {
+#[derive(Debug, Clone, Hash, PartialEq, PartialOrd, Eq, Ord)]
+enum HandJ {
+    HighCard(Vec<CardJ>),
+    OnePair(Vec<CardJ>),
+    TwoPair(Vec<CardJ>),
+    ThreeOfAKind(Vec<CardJ>),
+    FullHouse(Vec<CardJ>),
+    FourOfAKind(Vec<CardJ>),
+    FiveOfAKind(Vec<CardJ>),
+}
 
+fn recognize_hand_with_joker(cards: Vec<CardJ>) -> HandJ {
+    let mut card_counter: Counter<CardJ> = cards.iter().cloned().collect();
+    if card_counter[&CardJ::J] > 0 {
+        let target = card_counter.iter()
+            .filter(|(c, _n)| c != &&CardJ::J)
+            .fold((0, CardJ::J), |acc, (c, &n)| {
+                if acc.0 < n {
+                    (n, c.clone())
+                } else if acc.0 == n {
+                    (n, max(c.clone(), acc.1))
+                } else {
+                    acc
+                }
+            });
+        let target = if target.1 == CardJ::J {
+            CardJ::A
+        } else {
+            target.1
+        };
+
+        card_counter[&target] += card_counter[&CardJ::J];
+        card_counter[&CardJ::J] = 0
+    }
+    match card_counter.iter().filter(|(_c, &n)| n > 0).count() {
+        1 => HandJ::FiveOfAKind(cards),
+        2 => {
+            if card_counter.values().max().unwrap() == &4 {
+                HandJ::FourOfAKind(cards)
+            } else {
+                HandJ::FullHouse(cards)
+            }
+        },
+        3 => {
+            if card_counter.values().max().unwrap() == &3 {
+                HandJ::ThreeOfAKind(cards)
+            } else {
+                HandJ::TwoPair(cards)
+            }
+        },
+        4 => HandJ::OnePair(cards),
+        5 => HandJ::HighCard(cards),
+        _ => unreachable!()
+    }
+}
+
+
+impl FromStr for HandJ {
+    type Err = AdventError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cards: Vec<CardJ> = s.chars().map(|c| c.to_string().parse()).collect::<Result<_, AdventError>>()?;
+        Ok(recognize_hand_with_joker(cards))
+    }
+}
+
+pub fn run() -> (usize, usize) {
     let input = data_str!("day07");
     let hands_and_bids = parse(input).expect("invalid input");
+    let hands_and_bids_with_joker = parse_with_joker(input).expect("invalid input");
 
     (
         total_winnings(hands_and_bids),
-        0
+        total_winnings(hands_and_bids_with_joker)
     )
 }
 
@@ -93,7 +193,16 @@ fn parse(s: &str) -> Result<Vec<(Hand, u32)>, AdventError> {
     }).collect()
 }
 
-fn total_winnings(mut hands_and_bids: Vec<(Hand, u32)>) -> usize {
+fn parse_with_joker(s: &str) -> Result<Vec<(HandJ, u32)>, AdventError> {
+    split_lines(s).iter().map(|line| {
+        let parts: Vec<&str> = line.split_ascii_whitespace().collect();
+        let hand: HandJ = parts[0].parse()?;
+        let bid: u32 = parts[1].parse()?;
+        Ok((hand, bid))
+    }).collect()
+}
+
+fn total_winnings<H: Ord>(mut hands_and_bids: Vec<(H, u32)>) -> usize {
     hands_and_bids.sort();
     hands_and_bids.into_iter()
         .enumerate()
@@ -116,7 +225,9 @@ mod tests {
         ";
 
         let hands_and_bids = parse(input).expect("invalid input");
+        let hands_and_bids_with_joker = parse_with_joker(input).expect("invalid input");
 
         assert_eq!(total_winnings(hands_and_bids), 6440);
+        assert_eq!(total_winnings(hands_and_bids_with_joker), 5905);
     }
 }
